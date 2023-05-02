c02524e9e598f8ba57e21c65b5561ed5b8466ef4
wordworld <stone.zh@foxmail.com>
2023-05-02 22:25:34 +0800

    添加 man 命令查看帮助文档

--- alias.bat
+++ alias.bat
@@ -8,4 +8,5 @@
 @doskey which=where $*
 @doskey file=file -m %SHELL4WIN%file\share\misc\magic $*
 @doskey find=%SHELL4WIN%findutils\find $*
+@doskey  man=%SHELL4WIN%findutils\find %SHELL4WIN:\=/% -type f -regex ".*/man.*\b$1.*" -exec mandoc {} ; $2 $3 $4 $5 $6 $7 $8 $9
 @doskey date=%SHELL4WIN%coreutils\date $*
