24a85eef9be2c7031bc573b585c0c94bdc6554d5
wordworld <fstone.zh@foxmail.com>
2023-05-05 21:17:08 +0800

    定义 TERM 环境为 xterm-256color

--- win-bash/bashrc.sh
+++ win-bash/bashrc.sh
@@ -3,6 +3,7 @@ export SHELL4WIN=`echo ${SHELL4WIN}| tr '\' '/'`
 alias find=${SHELL4WIN}findutils/find
 alias whoami=${SHELL4WIN}coreutils/whoami
 
+export TERM=xterm-256color
 export PS1="[$(whoami)@$(hostname) \w]\$ "
 export HOME=$USERPROFILE
 
