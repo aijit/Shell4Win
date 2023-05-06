ed51e4630d87628d71b9dd3f4d24f6228e75d645
wordworld <fstone.zh@foxmail.com>
2023-05-06 21:37:01 +0800

    用户 PATH 变量优先于系统 PATH 出现在 shell PATH 中，以防止 shell 命令调用 windows 同名程序（如 find、whoami）

--- bin/wsh.bat
+++ bin/wsh.bat
@@ -1,8 +1,29 @@
 @echo off
+setlocal enabledelayedexpansion
 
-SET SHELLDIR=%SHELL4WIN%win-bash
+SET SHELLDIR=%SHELL4WIN:\=/%win-bash
 SET SHELL=%SHELLDIR%/bash.exe
+SET BASH_ENV=%SHELLDIR%/bashrc.sh
 SET BASH=%SHELL%
+
+:: PATH = usrPATH + sysPATH
+for /f "tokens=2 delims==" %%a in (
+    'wmic ENVIRONMENT where "UserName='%USERDOMAIN%\\%USERNAME%' and name='PATH'" get VariableValue /value'
+) do set usrPath=%%a
+for /f "delims=" %%a in ('echo "%usrPath%"') do set usrPath=%%a
+
+for /f "tokens=2 delims==" %%a in (
+    'wmic ENVIRONMENT where "UserName='<system>' and name='PATH'" get VariableValue /value'
+) do set sysPath=%%a
+for /f "delims=" %%a in ('echo "%sysPath%"') do set sysPath=%%a
+
+SET PATH=!usrPath! !sysPath!
+SET PATH=!PATH:~1,-1!
+
+set cwd=%cd%
+if not "%USERPROFILE%" == "%cwd%" (
+    set CURRENT_DIR=%cwd:\=/%
+)
 
 "%BASH%" ^
     -rcfile %SHELLDIR%/bashrc.sh ^
--- win-bash/bashrc.sh
+++ win-bash/bashrc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export SHELL4WIN=`echo ${SHELL4WIN}| tr '\' '/'`
-alias find=${SHELL4WIN}findutils/find
-alias whoami=${SHELL4WIN}coreutils/whoami
 
 export TERM=xterm-256color
 export PS1="[$(whoami)@$(hostname) \w]\$ "
