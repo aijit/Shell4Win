f5c3c861d900bcf335ece9e80571b0447f7ec9f8
wordworld <fstone.zh@foxmail.com>
2023-05-09 07:53:37 +0800

    man 命令只显示第一个匹配的帮助文档

--- mdocml/man.bat
+++ mdocml/man.bat
@@ -1,5 +1,10 @@
-@for /f %%m in ('%SHELL4WIN%findutils\find %SHELL4WIN:\=/% -type f -regex ".*/man.*\b%1.*"') do set manfiles=%%m
-@if "" == "%manfiles%" goto endofman
-@mandoc %manfiles% | less
-@set manfiles=
+@echo off
+setlocal enabledelayedexpansion
+for /f %%m in ('%SHELL4WIN%findutils\find %SHELL4WIN:\=/% -type f -regex ".*/man.*\b%1.*"') do (
+    set /a ln+=1
+    if !ln! == 1 set manfiles=%%m
+)
+if "" == "%manfiles%" goto endofman
+mandoc %manfiles% | less
+set manfiles=
 :endofman
