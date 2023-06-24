aa7901c9129d9705eba34f8850c1fcfd80d19d04
wordworld <fstone.zh@foxmail.com>
2023-06-24 12:21:47 +0800

    ssh alias 支持 trzsz -d 模式

--- alias.bat
+++ alias.bat
@@ -2,6 +2,12 @@
 ::     HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor
 ::     AutoRun=dir/to/alias.bat
 :: cmd.exe will auto run this script first, when open a new terminal
+@echo off
 @doskey alias=doskey /macros
 @doskey find=%SHELL4WIN%findutils\find $*
 @doskey date=%SHELL4WIN%coreutils\date $*
+where trzsz >nul 2>nul
+if %errorlevel% equ 0 (
+    doskey ssh=trzsz -d ssh $*
+)
+@echo on
